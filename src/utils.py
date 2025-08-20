7c6810945bcbc2e778c0e5af5bc89aff24b7fe26
126MANGESH <mangeshjadhav126@gmail.com>
2025-08-20 09:25:57 +0530

    hyperpara meter

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
  
 
 
@@ -25,8 +26,15 @@ def evaliate_models(x_train, y_train, x_test, y_test, models,param):
        for i in range(len(list(models))):
           model_name = list(models.keys())[i]
           model = models[model_name]
-        #   model = list(models.values())[i]
+          para = param[model_name]
+
+          gs = GridSearchCV(model,para,cv=3)
+          gs.fit(x_train, y_train)
+          model.set_params(**gs.best_params_)
           model.fit(x_train, y_train)
+          
+        #   model = list(models.values())[i]
+        #   model.fit(x_train, y_train)
           y_train_pred = model.predict(x_train)
           y_test_pred = model.predict(x_test)
           train_model_score =r2_score(y_train,y_train_pred)
